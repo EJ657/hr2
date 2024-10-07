019f0f586e98daf455c4d48e8557d1e2a671fdef
EJ657 <ejdelosreyes657@gmail.com>
2024-10-07 23:21:13 +0800

    Update connection string

--- connection.php
+++ connection.php
@@ -1,11 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root"; // MySQL username
-$password = ""; // MySQL password (leave empty for default setup)
-$dbname = "employee_management"; // Database name
+$hostname = "localhost";
+$username = "hr2_user"; // MySQL username
+$password = "BOCi!-v!#KkoPP+y"; // MySQL password (leave empty for default setup)
+$dbname = "hr2_db"; // Database name
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect($hostname, $username, $password, $dbname);
 
 // Check connection
 if (!$conn) {
